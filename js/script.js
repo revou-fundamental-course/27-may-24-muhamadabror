c456bb628ba6eff6020c01a1f56c39b0b076f6e3
muhamadabror <aanfarodis41@gmail.com>
2024-06-04 23:29:31 +0700

    perubahan ketujuh

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
-// Ini javascriptt
-
+// // // Ini javascriptt
+// document.addEventListener("DOMContentLoaded", function () {
+//     const nameSpan = document.getElementById("tag-name");
+//     const name = prompt("Nama anda: ", "");
+    
+//     if (name) { nameSpan.textContent = name; } });
 
