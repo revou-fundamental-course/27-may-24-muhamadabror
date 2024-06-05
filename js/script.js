3a39641560a3ea5d85364a6e78d3701e221ad7c9
muhamadabror <aanfarodis41@gmail.com>
2024-06-06 00:13:40 +0700

    perubahan ke tujuh

--- js/script.js
+++ js/script.js
@@ -1,7 +1,30 @@
-// // // Ini javascriptt
-// document.addEventListener("DOMContentLoaded", function () {
-//     const nameSpan = document.getElementById("tag-name");
-//     const name = prompt("Nama anda: ", "");
-    
-//     if (name) { nameSpan.textContent = name; } });
+// Ini javascriptt
 
+// popup pengisian data nama
+document.addEventListener("DOMContentLoaded", function () {
+    const nameSpan = document.getElementById("tag-name");
+    const name = prompt("Nama anda: ", "");
+   
+    if (name) { nameSpan.textContent = name; } });
+// untuk menghilangkan pengulangan tombol submit
+document.getElementById('form-group').addEventListener('submit'), function(e){
+    e.preventDefault();
+
+// melakukan validasi form
+function validasiForm (){
+    const nama = document.getElementsById('name').value;
+    const email = document.getElementsById('email').value;
+    const dateBorn = document.getElementsById('date-born').value;
+    const jenisKelamin = document.querySelector('input[name="gender"]:checked').value
+    const message = document.getElementById('pesan').value;
+
+    if (nama == "" && email == "" && dateBorn == "" && jenisKelamin == "" && message == "" ){
+        alert('Tidak boleh ada yang kosong');
+        return false;
+    }; 
+    
+};
+// hasil dari pengisian form
+function setSender(nama, email, dateBorn, jenisKelamin, message ){
+
+}};
