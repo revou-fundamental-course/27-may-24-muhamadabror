d388b1002a6ebbcf0c56bd00be144c03d2e1ce11
muhamadabror <aanfarodis41@gmail.com>
2024-06-06 21:51:04 +0700

    Mini Project

--- js/script.js
+++ js/script.js
@@ -6,25 +6,39 @@ document.addEventListener("DOMContentLoaded", function () {
     const name = prompt("Nama anda: ", "");
    
     if (name) { nameSpan.textContent = name; } });
-// untuk menghilangkan pengulangan tombol submit
-document.getElementById('form-group').addEventListener('submit'), function(e){
-    e.preventDefault();
 
-// melakukan validasi form
-function validasiForm (){
+document.getElementById('form').addEventListener('submit', function(e){
+    e.preventDefault();
+    
     const nama = document.getElementsById('name').value;
     const email = document.getElementsById('email').value;
     const dateBorn = document.getElementsById('date-born').value;
-    const jenisKelamin = document.querySelector('input[name="gender"]:checked').value
+    const jenisKelamin = document.querySelector('input[name="gender"]:checked').value;
     const message = document.getElementById('pesan').value;
+    const hasil = 'Nama: ${nama}\nTanggal lahir: ${dateBorn}\nEmail: ${email}\nPesan: ${message}'
 
-    if (nama == "" && email == "" && dateBorn == "" && jenisKelamin == "" && message == "" ){
-        alert('Tidak boleh ada yang kosong');
-        return false;
-    }; 
+    document.getElementById('result-form') = hasil;
     
-};
-// hasil dari pengisian form
-function setSender(nama, email, dateBorn, jenisKelamin, message ){
+         
+});
 
-}};
+
+// melakukan validasi form
+
+
+
+    // form.addEventListener('submit', (e)=>{
+    //     if (nama == null && email == null && dateBorn == null && jenisKelamin == null && message == null ){
+            
+    //         e.preventDefault();
+    //     }
+        
+    // })
+      
+
+
+
+// hasil dari pengisian form
+// function setSender(nama, email, dateBorn, jenisKelamin, message ){
+
+// };
